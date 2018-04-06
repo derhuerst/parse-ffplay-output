ecf3c9a88ad5a5a44a78c5c293aa74e546b87663
Jannis R <mail@jannisr.de>
2018-04-06 04:05:22 +0200

    wip: test

--- example.js
+++ example.js
@@ -6,11 +6,7 @@ const pty = require('node-pty')
 
 const createParser = require('.')
 
-if (!process.argv[2]) {
-	console.error('Missing file path.')
-	process.exit(1)
-}
-const file = path.join(__dirname, process.argv[2])
+const file = path.join(__dirname, '440-hz.ogg')
 const shell = platform() === 'win32' ? 'powershell.exe' : 'bash'
 
 const sub = pty.spawn(shell, [], {
@@ -21,7 +17,7 @@ const sub = pty.spawn(shell, [], {
 	env: process.env
 })
 
-sub.write(`ffplay -vn -volume 0 -hide_banner -stats -nodisp '${file}'; exit\n`)
+sub.write(`ffplay -vn -hide_banner -stats -nodisp -autoexit '${file}'; exit\n`)
 
 const parser = createParser()
 sub.pipe(parser)
--- test.js
+++ test.js
@@ -1,7 +1,37 @@
 'use strict'
 
+const path = require('path')
+const {platform} = require('os')
 const test = require('tape')
+const pty = require('node-pty')
 
 const createParser = require('.')
 
-// todo
+const file = path.join(__dirname, '440-hz.ogg')
+const shell = platform() === 'win32' ? 'powershell.exe' : 'bash'
+
+test('emits a reasonable progress value', {timeout: 10 * 1000}, (t) => {
+	const sub = pty.spawn(shell, [], {
+		name: 'parse-ffplay-output',
+		cols: 80,
+		rows: 30,
+		cwd: process.cwd(),
+		env: process.env
+	})
+
+	const parser = createParser()
+	sub.pipe(parser)
+	sub.write(`ffplay -vn -hide_banner -stats -nodisp -autoexit '${file}'; exit\n`)
+
+	const validProgress = (expected) => () => {
+		const p = parser.getProgress()
+		t.equal(typeof p, 'number')
+		t.notOk(Number.isNaN(p))
+		t.ok(Math.round(p), expected)
+	}
+
+	t.plan(3 * 3)
+	setTimeout(validProgress(1), 1 * 1000)
+	setTimeout(validProgress(2), 2 * 1000)
+	setTimeout(validProgress(8), 8 * 1000)
+})
